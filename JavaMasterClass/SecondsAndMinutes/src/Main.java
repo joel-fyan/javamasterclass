26ebce4b29eb73eaa3d8c9c7cf2ce828317e1ae5
Joel Fyan <joel.r.fyan@hp.com>
2019-10-01 12:56:55 -0600

    Refined exercise

--- JavaMasterClass/SecondsAndMinutes/src/Main.java
+++ JavaMasterClass/SecondsAndMinutes/src/Main.java
@@ -3,7 +3,13 @@ public class Main {
         System.out.println("Duration -1 minutes and 2 seconds: " + getDurationString(-1, 2)); // Expect "Invalid value"
         System.out.println("Duration 1 minute and -1 seconds: " + getDurationString(1, -1)); // Expect "Invalid value"
         System.out.println("Duration 1 minute and 60 seconds: " + getDurationString(1, 60)); // Expect "Invalid value"
+        System.out.println("Duration 27 minutes and 21 seconds: " + getDurationString(27,21)); // Expect "0h 27m 21s"
         System.out.println("Duration 121 minutes and 24 seconds: " + getDurationString(121,24)); // Expect "2h 1m 24s"
+
+        System.out.println("Duration 60 seconds: " + getDurationString(60)); // Expect "0h 1m 0s"
+        System.out.println("Duration 21 seconds: " + getDurationString(21)); // Expect "0h 0m 21s"
+        System.out.println("Duration 121 seconds: " + getDurationString(121)); // Expect "0h 2m 1s"
+        System.out.println("Duration 3741 seconds: " + getDurationString(3741)); // Expect "1h 2m 21s"
     }
 
     private static String getDurationString(int minutes, int seconds)
@@ -15,6 +21,23 @@ public class Main {
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
 
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+        return getFormattedValue(hours) + "h " + getFormattedValue(remainingMinutes) + "m " + getFormattedValue(seconds) + "s";
+    }
+
+    private static String getDurationString(int seconds)
+    {
+        if(seconds < 0){
+            return "Invalid value";
+        }
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+
+    private static String getFormattedValue(int value)
+    {
+        return String.format("%02d", value);
     }
 }
