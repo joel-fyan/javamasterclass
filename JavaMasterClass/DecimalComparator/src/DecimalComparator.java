43b3dd1a4f43f823ef41db1a508d7e29e01e8587
Joel Fyan <joel.r.fyan@hp.com>
2019-10-01 11:17:58 -0600

    Refined

--- JavaMasterClass/DecimalComparator/src/DecimalComparator.java
+++ JavaMasterClass/DecimalComparator/src/DecimalComparator.java
@@ -1,9 +1,6 @@
 class DecimalComparator {
     static boolean areEqualByThreeDecimalPlaces(double first, double second)
     {
-        int firstInt = (int)(first * 1000);
-        int secondInt = (int)(second * 1000);
-
-        return firstInt == secondInt;
+        return (int)(first * 1000) == (int)(second * 1000);
     }
 }
