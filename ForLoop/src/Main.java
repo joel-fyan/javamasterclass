029d54f08568ee132bb157ee6b16426feb4dc27c
Joel Fyan <joel.fyan@gmail.com>
2019-10-04 16:06:42 -0600

    Another exercise

---
+++ ForLoop/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000d, i));
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
