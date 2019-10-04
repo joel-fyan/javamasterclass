b2cc72de38d303654d84d40fb551d2cc67011370
Joel Fyan <joel.fyan@gmail.com>
2019-10-04 16:11:35 -0600

    Same exercise refined

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         for(int i = 2; i < 9; i++) {
-            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000d, i));
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000d, i)));
+        }
+
+        System.out.println();
+        System.out.println("And now backwards...");
+        for(int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f",calculateInterest(10000d, i)));
         }
     }
 
